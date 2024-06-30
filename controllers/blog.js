fcc0f9a7dc54dc759870482be6607554adb1f8de
Erlend <erlend.eelmets@gmail.com>
2024-06-30 15:47:18 +0300

    4.2

---
+++ controllers/blog.js
@@ -0,0 +1,21 @@
+const blogRouter = require("express").Router();
+const Blog = require("../models/blog");
+
+blogRouter.get("/", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+
+blogRouter.post("/", (request, response, next) => {
+  const blog = new Blog(request.body);
+
+  blog
+    .save()
+    .then((result) => {
+      response.status(201).json(result);
+    })
+    .catch((error) => next(error));
+});
+
+module.exports = blogRouter;
