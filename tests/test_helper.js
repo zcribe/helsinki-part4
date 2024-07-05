372219cf772342dbff553fcd1c9d936e8b40d841
Erlend <erlend.eelmets@gmail.com>
2024-07-05 10:01:26 +0300

    Better fake data

--- tests/test_helper.js
+++ tests/test_helper.js
@@ -1,12 +1,12 @@
 const Blog = require('../models/blog')
 
 const initialBlogs = [
-  { title: 'Asdi', author: 'Meat', likes: 3, id: 5, url: 'fsdf' },
-  { title: 'Asasdasdi', author: 'Medasdaat', likes: 3, id: 56, url: 'dasda' },
+  { title: 'Legacy Optimization Executive', author: 'David Pouros', likes: 9, id: 5, url: 'https://dimwitted-symmetry.info' },
+  { title: 'Human Tactics Supervisor', author: 'Doyle Mayer DVM', likes: 7, id: 56, url: 'http://vast-extinction.net' },
 ]
 
 const nonExistingId = async () => {
-  const blog = new Blog({ author: '3123', title: 'adsda', url: 'dasda' })
+  const blog = new Blog({ author: 'Marjorie West', title: 'Chief Marketing Planner', url: 'http://sturdy-blowhole.info' })
   await blog.save()
   await blog.deleteOne()
 
